4577dd14b09a7ad1206a4d0ac1babe61173517bb
Akmal Xayrullayev <akmalweb.dev@gmail.com>
2026-04-14 10:19:48 +0500

    Updated database connection string for production

--- Task4WebApp/obj/Debug/net8.0/Task4WebApp.AssemblyInfo.cs
+++ Task4WebApp/obj/Debug/net8.0/Task4WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task4WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98382d8a843ea0b5bd3e289e72ec0e4a3b04869e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task4WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task4WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
